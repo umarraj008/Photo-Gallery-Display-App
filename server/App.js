3f69022367d3b72da38cfcf9b78a2cc5033fdf42
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2023-11-21 19:50:17 +0000

    App.js commenting + todo
    - finished commenting app.js
    - added todo file to better keep track of TODO

--- server/App.js
+++ server/App.js
@@ -48,20 +48,23 @@ module.exports = class App {
     }
 
     /**
-     * Handles Express server setup  
+     * Handles Express server setup.  
      * @returns {} Nothing is returned.
      */
     startServer() {
         // Creating server
         this.server = this.app.listen(this.port, () => {
+            // Clear console
             console.clear();
             
+            // Applicaiton starting logs
             this.log(PROJECT.name + " v" + PROJECT.version + " By " + PROJECT.author);
             this.log(PROJECT.description + "\n");
             this.log("Application is running in: " + process.env.NODE_ENV + "mode \n");
             this.log("Server has started.");
             this.log("Listening at: " + this.address + ":" + this.port);
 
+            // Start main interval for photos loop
             this.intervalID = setInterval(() => {
                 this.photos();
             }, this.photoInterval);
@@ -81,47 +84,71 @@ module.exports = class App {
     }
 
     /**
-     *   
+     * This starts the Socket.IO server and provides listeners for communication events that occur.  
      * @returns {} Nothing is returned.
      */
     startSocketIOServer() {
+        // Socket.IO server
         this.io = require("socket.io")(this.server);
 
+        // Socket connection listener
         this.io.sockets.on("connection", (socket) => {
+
+            // When device connects
             this.log("Device is Connected to Server");
+            
+            // Check if applicaiton is running in dev environment
             if (process.env.NODE_ENV.trim() === "DEV") {
+                // Send always on to stop viewer from display off
                 this.sendToAll("always-on");
             }
+
+            // Send project version and current image
             this.sendToAll("project-version", PROJECT.version);
             this.sendToAll("update-image", this.images[this.currentImage]);
             
+            // When device is disconnected
             socket.on("disconnect", () => {
                 this.log("Device is Disconnected to Server");
             });
             
+            // When admin presses "refresh" button
             socket.on("admin-refresh", () => {
                 this.sendToAll("refresh");
                 this.log("Refreshing Devices");
             });
             
+            // When admin presses "admin refresh" button
             socket.on("admin-admin-refresh", () => {
                 this.sendToAll("refresh-admin");
                 this.log("Refreshing Admin Devices");
             });
             
+            // When admin presses "stop server" button
             socket.on("admin-stop", () => {
+                // Tell devices to refresh
                 this.sendToAll("refresh");
                 this.sendToAll("refresh-admin");
                 this.log("Stopping Server");
+
+                // Exit application
                 process.exit();
             });
             
+            // When admin presses "restart server" button
             socket.on("admin-restart-server", () => {
+                // Tell devices to refresh
                 this.sendToAll("refresh");
                 this.sendToAll("refresh-admin");
+
+                // Stop photos inverval
                 clearInterval(this.intervalID);
                 // this.io.server.close();
+                
+                // Close express server
                 this.server.close();
+
+                // Log and emit restart event
                 this.log("Restarting Server");
                 this.em.emit("restart");                
             });
@@ -129,7 +156,9 @@ module.exports = class App {
     }
 
     /**
-     *   
+     * Function to emit Socket.IO message to all clients.
+     * @param {String} msg This will be the message you would like to send to clients that they will listen for.
+     * @param {any} data This will be any data associated with your message.
      * @returns {} Nothing is returned.
      */
     sendToAll(msg, data) {
@@ -160,13 +189,15 @@ module.exports = class App {
     }
     
     /**
-     *   
+     * This funciton itterates the current photo and updates all clients that the current photo has changed.  
      * @returns {} Nothing is returned.
      */
     photos() {
+        // Update current image
         this.currentImage++;
         if (this.currentImage >= this.images.length) this.currentImage = 0;
 
+        // Update clients that current image has changed
         this.sendToAll("update-image", this.images[this.currentImage]);
         this.log("Updating Image: " + (this.currentImage+1));
     }
--- server/main.js
+++ server/main.js
@@ -18,23 +18,3 @@ em.on("restart", () => {
     main.run();
 
 });
-
-//+========================
-// TODO
-//+========================
-// instead of manually adding images to list => read from folder
-// Classify the app so server can restart
-// update interval from controller or admin
-// add image from controller
-// check added images for dupliactes in list
-// randomize/shuffle image order
-// enable disable image updates in admin console
-// edit shutdown time config
-// manage images in admin
-// brightness on certain times
-// photo transision
-// show time
-// show timeline of photos in controller/admin
-// display if not connected to server
-// view server cpu/ram/storage stats
-// Getting node env to make display always on if debug mode
